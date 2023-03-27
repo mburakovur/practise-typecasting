7a5e6cad862f940c55b39f90c8f8145409584b2e
M. Burak Ovur <mburak.ovur@gmail.com>
2023-03-27 10:35:59 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Type casting!");
+
+        // Implicit Casting
+        // char -> int -> long -> float -> double
+
+        Console.WriteLine("Implicit Casting");
+        
+        long b = 24;
+        float c;
+        c = b;
+        Console.WriteLine(c);
+
+        // Explicit Casting
+        Console.WriteLine("Explicits Casting");
+        float d = 14.8f;
+        long e = (long)d;
+        Console.WriteLine(e);
+
+
+        // Type Conversion Methods
+        Console.WriteLine("Type Conversion Methods");
+        string f = "24";
+        double g = 19.87;
+        int num01 = Convert.ToInt32(f);
+        int num02 = Convert.ToInt32(g);
+
+        int result = num01 + num02;
+        Console.WriteLine(result);
+
+        // Parse
+        Console.WriteLine("Parse");
+
+        string code = "15689413";
+        int intCode = Int32.Parse(code);
+        Console.WriteLine(intCode);
